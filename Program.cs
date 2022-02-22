fb7fb2d8effa7bc78fbdf8713f8f103060033e1d
Henrique Alves <99695063+A1veszera@users.noreply.github.com>
2022-02-22 18:24:11 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoGit2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
